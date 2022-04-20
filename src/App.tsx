0af0d3b81310f18fed833fd3d0d5bd069b51f357
jobayed003 <jobayed803@gmail.com>
2022-04-21 04:36:19 +0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import React from 'react';
 import './App.scss';
 
 const App: React.FC = () => {
-  return <div className="hello">Hey</div>;
+  return (
+    <div className="hello">
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </div>
+  );
 };
 
 export default App;
